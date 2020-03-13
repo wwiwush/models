ad0f58b5df91c504cedb463434efaf3286851c51
wuwei <bobbycharles846@hotmail.com>
2020-03-13 11:24:05 +0800

    path change

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 cd models && git pull -q && cd ..
 showmount -e 192.168.50.216
 mount -t nfs 192.168.50.216:/home/blita/nfs/mlruns /tmp/mlruns
-export PYTHONPATH=$PYTHONPATH:/tmp/models/research:/tmp/models/research/slim
-python /tmp/models/research/object_detection/model_main.py --pipeline_config_path=./object_detection/samples/configs/ssd_mobilenet_v1_focal_loss_pets.config --model_dir=object_detection/checkpoint --num_train_steps=50000 --sample_1_of_n_eval_examples=1 --alsologtostder
+cd /tmp/models/research
+export PYTHONPATH=$PYTHONPATH:pwd:pwd/slim
+python ./object_detection/model_main.py --pipeline_config_path=./object_detection/samples/configs/ssd_mobilenet_v1_focal_loss_pets.config --model_dir=object_detection/checkpoint --num_train_steps=50000 --sample_1_of_n_eval_examples=1 --alsologtostder
