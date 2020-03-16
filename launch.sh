8e76d6ae18ed5a8bcd1a5a898742d47f1aff1187
wuwei <bobbycharles846@hotmail.com>
2020-03-16 19:03:44 +0800

    no interaction and terminal,execute the python command directly

--- launch.sh
+++ launch.sh
@@ -1,1 +1,3 @@
 cd /tmp/models && git pull -q && cd research
+export PYTHONPATH=$PYTHONPATH:pwd:pwd/slim
+python ./object_detection/model_main.py --pipeline_config_path=./object_detection/samples/configs/ssd_mobilenet_v1_focal_loss_pets.config --model_dir=object_detection/checkpoint --num_train_steps=50000 --sample_1_of_n_eval_examples=1 --alsologtostder
