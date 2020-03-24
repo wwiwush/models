e3fd4e3b887041ae9320516b7fcd4e7213c645db
wuwei <bobbycharles846@hotmail.com>
2020-03-24 16:55:44 +0800

    default command without flexible params

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 cd /tmp/models && git pull -q && cd research
 mount -t nfs 192.168.50.216:/home/blita/nfsnew/nfstest /tmp/nfsnew
 export PYTHONPATH=$PYTHONPATH:pwd:pwd/slim
+python ./object_detection/model_main.py --pipeline_config_path=./object_detection/samples/configs/ssd_mobilenet_v1_focal_loss_pets.config --model_dir=./object_detection/checkpoint --alsologtostder
